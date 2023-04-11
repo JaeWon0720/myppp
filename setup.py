9d771c91275b6c8c83d6133b56369b6f3ac7c92a
JaeWon0720 <126738978+JaeWon0720@users.noreply.github.com>
2023-04-11 13:11:08 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='wjdwodnjs01',
+    version='0.0.1',
+    desciption='nice calculator',
+    author='jaewon JUNG',
+    url="https://github.com/JaeWon0720/myppp",
+    download_url="https://github.com/JaeWon0720/myppp/archive/refs/tags/v0.0.1.zip",
+    packages=['myppppppp'],
+    classifiers=[
+       " Programming Language :: python :: 3",
+    ],
+)
